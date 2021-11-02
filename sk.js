60d73136047e702fb25a6a8898c7f72fb8841b04
devsk <91372631+devskcode@users.noreply.github.com>
2021-11-02 13:51:22 +0500

    Add files via upload

---
+++ sk.js
@@ -0,0 +1,70 @@
+console.log("this is working");
+var day=4;
+switch(day){
+    case 1:
+    document.write("monday");
+    break;
+    case 2:
+    document.write("tuesday");
+    break;
+    case 3:
+    document.write("wednesday");
+    break;
+    case 4:
+        document.write("thuresday");
+    break;
+    case 5:
+        document.write("friday");
+    break;
+    case 6:
+        document.write("satureday");
+    break;
+    case 7:
+        document.write("sunday");
+    break;
+    default:
+    document.write("enter the valid day");                
+}
+document.write("<br>");
+var months=5;
+switch(months){
+    case 1:
+        document.write("january");
+    break;
+    case 2:
+        document.write("febuary");
+    break;
+    case 3:
+        document.writed("mrach");
+    break;
+    case 4:
+        document.write("apiral");
+    break;
+    case 5:
+        document.write("may");
+    break;
+    case 6:
+        document.write("june");
+    break;
+    case 7:
+        documentS.write("july");
+    break;
+    case 8:
+        document.write("august");
+    break;
+    case 9:
+        document.write("september");
+    break;
+    case 10:
+        document.write("october");
+    break;
+    case 11:
+        document.write("november");
+    break;
+    case 12:
+        document.write("december");
+    break;
+    default:
+        document.write("enter the valid value");                            
+
+}
