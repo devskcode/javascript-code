af48395435ee9d04727100f46f03c9ba8ec4e493
devsk <91372631+devskcode@users.noreply.github.com>
2021-11-14 17:29:14 +0500

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,22 @@
+console.log("sobia"); 
+for(var a = 1; a<=100; a=a+10){
+    for(var b = a; b< a+10; b++ ){
+document.write(b+ " ");
+    }
+
+    document.write("<br>"); 
+}
+for(var a=1; a<=5;  a++){
+    for(var b=1; b<=a; b++){
+    document.write(b+"");    
+    }
+    document.write("<br>");
+}
+for(var a=5; a<=10; a++){
+    for(var b=5; b<=a; b++){
+        document.write(b);
+    }
+    document.write("<br>");
+}
+
+
