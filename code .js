5a7dcb8dbf1e5d5928e0038a788d5e573e19305e
devsk <91372631+devskcode@users.noreply.github.com>
2022-01-01 18:53:09 +0500

    Add files via upload

---
+++ code .js
@@ -0,0 +1,17 @@
+console.log("tariq");
+var a =  ["usman","arslan","zeeshan"];
+document.write(a + "<br>");
+a.sort();
+document.write(a + "<br>");
+document.write("<br>");
+var b=  ["natasha","arslan","warisha","tariq","jawaria","amara","sobia","nadia"];
+document.write(b + "<br>");
+b.sort();
+document.write(b + "<br>");
+document.write("<br>");
+var c = ["school","college"];
+document.write(c + "<br><br>");
+c.pop();
+document.write(c +"<br><br>");
+c.push("university");
+document.write(c + "<br><br>");
