02d518c11ff3e04a32dc3a1b42687782b26c1b2e
devsk <91372631+devskcode@users.noreply.github.com>
2022-10-11 09:42:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+document.write("sobia");
+var a;
+a=1;
+console.log(a);
+var b;
+b=333;
+console.log(b);
+var age;  
+age = "zee";
+console.log(age);
+var age =45;
+console.log(age);
+age =true;
+console.log(age);
+var age =45;
+var name ="hello";
+var feepaid=true;
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof feepaid);
+
+
+
+
+
+
+
+
+
+
+
+
+
