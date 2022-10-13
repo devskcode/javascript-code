bcba12bca22b47f85ebeec9b21fc9d90f70ef7ac
devsk <91372631+devskcode@users.noreply.github.com>
2022-10-13 00:35:11 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,43 @@ var feepaid=true;
 console.log(typeof age);
 console.log(typeof name);
 console.log(typeof feepaid);
-
+//var rose = "Do you like this website";
+//alert (rose);
+var a=2;
+    b=3;
+var c =a+b;
+var d =a-b;
+var e =a*b;
+var f =a/b;
+var g =a**b;
+var h =a%b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+var a= 5;
+ a+=4;
+ console.log(a);
+ var b= 4;
+  b-=1;
+  console.log(b);
+  var a = 2;
+  var b = 1;
+ c = ++a - ++b;
+ //var c = 3-2;
+ console.log(a);
+ console.log(b);
+ console.log(c);
+ var firstname ="natasha";
+ var lastname = "kanwal";
+ fullname = firstname +""+lastname; 
+console.log(fullname);
+var name =prompt("what is your name?" );
+console.log(name);
+var web =prompt("website want to be friend to you");
+console.log(web);
 
 
 
